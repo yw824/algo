29ced16374f1fadde1aa8c10cae8d1e5053f8553
yw824 <yongwoo25@gmail.com>
2024-01-09 23:26:26 +0900

    - 01753_dijkstra.py

---
+++ Baekjoon_Python/01753_dijkstra.py
@@ -0,0 +1,88 @@
+"""
+https://www.acmicpc.net/problem/1753
+
+방향그래프가 주어지면 주어진 시작점에서 다른 모든 정점으로의 최단 경로를 구하는 프로그램을 작성하시오.
+단, 모든 간선의 가중치는 10 이하의 자연수이다.
+
+첫째 줄에 정점의 개수 V와 간선의 개수 E가 주어진다. (1 ≤ V ≤ 20,000, 1 ≤ E ≤ 300,000)
+모든 정점에는 1부터 V까지 번호가 매겨져 있다고 가정한다.
+
+둘째 줄에는 시작 정점의 번호 K(1 ≤ K ≤ V)가 주어진다.
+셋째 줄부터 E개의 줄에 걸쳐 각 간선을 나타내는 세 개의 정수 (u, v, w)가 순서대로 주어진다.
+이는 u에서 v로 가는 가중치 w인 간선이 존재한다는 뜻이다. u와 v는 서로 다르며 w는 10 이하의 자연수이다.
+서로 다른 두 정점 사이에 여러 개의 간선이 존재할 수도 있음에 유의한다.
+
+첫째 줄부터 V개의 줄에 걸쳐, i번째 줄에 i번 정점으로의 최단 경로의 경로값을 출력한다.
+시작점 자신은 0으로 출력하고, 경로가 존재하지 않는 경우에는 INF를 출력하면 된다.
+
+5 6
+1
+5 1 1
+1 2 2
+1 3 3
+2 3 4
+2 4 5
+3 4 6
+
+0
+2
+3
+7
+INF
+"""
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+import heapq
+
+# 정점의 개수 n, 간선의 개수 m
+n, m = map(int, input().split())
+
+# 시작 정점의 번호
+k = int(input())
+
+# 무한을 의미하는 INF
+INF = int(1e9)
+
+# 그래프 초기화
+graph = [[] * (n+1) for _ in range(n+1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n+1)
+
+# 간선 정보 입력
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # a->b가 c비용
+    graph[a].append((b, c))
+
+
+def dijkstra(start):
+    q = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0, start))  # 거리 상으로 가장 작은 것을 넣는다.
+    distance[start] = 0
+
+    while q:
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+# 다익스트라 알고리즘을 수행
+dijkstra(k)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
